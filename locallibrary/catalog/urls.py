6f3fc4fda09fe46890fc97c920e006e047cdc433
jonnyprior <jonnyprior@hotmail.com>
2018-02-27 09:54:01 +0000

    Moz Part5 Done: Added homepage and generic templates

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+ # name used to uniquely indentify this URL. Can be used to reverse mapper. For example link pages to home using <a href="{% url 'index' %}">Home</a>
 urlpatterns = [
-
+    path('', views.index, name='index'),
 ]

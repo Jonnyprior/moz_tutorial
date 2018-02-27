ebfa0d7ddf0cd1132e3b8d8da47e470e23fd8d99
jonnyprior <jonnyprior@hotmail.com>
2018-02-27 14:56:26 +0000

    Moz part7 done: Use of sessions. Added visit counter for user

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Book, Author, BookInstance, Genre, Language
 
 
@@ -15,9 +16,28 @@ def index(request):
     num_genres = Genre.objects.all().count()
     books_containing_the = Book.objects.filter(title__contains="the").count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits=request.session.get('num_visits', 0) # Gets num_visits session key, default to 0 if not been set previously
+    request.session['num_visits'] = num_visits+1
+
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
-        context={'num_books':num_books, 'num_instances':num_instances, 'num_instances_available':num_instances_available, 'num_authors':num_authors, 'num_genres': num_genres, 'books_containing_the': books_containing_the},
+        context={'num_books':num_books, 'num_instances':num_instances, 'num_instances_available':num_instances_available, 'num_authors':num_authors,
+        'num_genres': num_genres, 'books_containing_the': books_containing_the, 'num_visits':num_visits},
     )
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 4
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
